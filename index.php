06a02d8556b54f0eb73bd2d12c1f2e59b6c24f11
ajivc17 <157089721+ajivc17@users.noreply.github.com>
2024-05-21 01:03:08 +0800

    index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (isset($_GET['delete']) && isset($_GET['task'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do List</title>
+    <title>To-Do List</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
